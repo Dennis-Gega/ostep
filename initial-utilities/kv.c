86a7cad166106d618bb22cfe0840cb7a2ad79909
dennis gega <dennis.gega@uconn.edu>
2025-11-16 20:04:48 -0500

    refactored input and opening files

--- initial-utilities/kv.c
+++ initial-utilities/kv.c
@@ -3,9 +3,14 @@
 #include <fcntl.h>
 #include <stdlib.h>
 
+void read_input(char **from , char **to);
+void open_file(char *filename, char *mode, FILE **to);
+
+
 int main(int argc, char* argv[])
 {
     char *command, *key, *value;
+    FILE *databasep;
 
     for (int i = 1; i < argc; i++) {
         command = strsep(&argv[i], ",");
@@ -15,22 +20,10 @@ int main(int argc, char* argv[])
              * put mode routine
              */
 
-            key = strsep(&argv[i], ",");
-            if (key == NULL || strcmp(key, "") == 0) {
-                fprintf(stderr, "usage: ./kv <mode>,<key>,<value> ...\n");
-                exit(EXIT_FAILURE);
-            }
-            value = strsep(&argv[i], ",");
-            if (value == NULL || strcmp(value, "") == 0) {
-                fprintf(stderr, "usage: ./kv <mode>,<key>,<value> ...\n");
-                exit(EXIT_FAILURE);
-            }
+            read_input(&argv[i], &key);
+            read_input(&argv[i], &value);
 
-            FILE *databasep = fopen("database.txt", "a");
-            if (databasep == NULL) {
-                fprintf(stderr, "error: fopen() call failed\n");
-                exit(EXIT_FAILURE);
-            }
+            open_file("database.txt", "a", &databasep);
 
             fprintf(databasep, "%s,%s\n", key, value);
             fclose(databasep);
@@ -40,24 +33,18 @@ int main(int argc, char* argv[])
              * get mode routine
              */
 
-            key = strsep(&argv[i], ",");
-            if (key == NULL || strcmp(key, "") == 0) {
-                fprintf(stderr, "usage: ./kv <mode>,<key>,<value> ...\n");
-            }
+            read_input(&argv[i], &key);
 
-            FILE *databasep = fopen("database.txt", "r");
-            if (databasep == NULL) {
-                fprintf(stderr, "error: fopen() call failed\n");
-                exit(EXIT_FAILURE);
-            }
+            open_file("database.txt", "r", &databasep);
 
             char *linep = NULL;
             size_t linecapp = 0;
             while (getline(&linep, &linecapp, databasep) != -1) {
                 char *db_key = strsep(&linep, ",");
-                if (strcmp(db_key, key)) {
-                    value = strsep(&linep, ","); 
+                if (strcmp(db_key, key) == 0) {
+                    value = strsep(&linep, "\n"); 
                     printf("%s\n", value);
+                    break;
                 }
             }
 
@@ -65,4 +52,24 @@ int main(int argc, char* argv[])
         }
     }
     return EXIT_SUCCESS;
+}
+
+void read_input(char **from , char **to)
+{
+    *to = strsep(from, ",");
+    if (*to == NULL || strcmp(*to, "") == 0) {
+        fprintf(stderr, "usage: ./kv <mode>,<key>,<value> ...\n");
+        exit(EXIT_FAILURE);
+    }
+    return;
+}
+
+void open_file(char *filename, char *mode, FILE **to)
+{
+    *to = fopen(filename, mode);
+    if (to == NULL) {
+        fprintf(stderr, "error: fopen() call failed\n");
+        exit(EXIT_FAILURE);
+    }
+    return;
 }
